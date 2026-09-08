3e9ab585cc77df213af4ff6c6bcff859e0710f30
Hiển Vinh <hthvinh48@gmail.com>
2026-09-09 04:50:25 +0700

    update on asm04

--- src/main/java/com/example/asm01/dto/CandidateCreateDTO.java
+++ src/main/java/com/example/asm01/dto/CandidateCreateDTO.java
@@ -3,6 +3,7 @@ package com.example.asm01.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class CandidateCreateDTO {
@@ -20,6 +21,12 @@ public class CandidateCreateDTO {
 
     @Min(0)
     private Integer yearsOfExperience;
+
+    @Pattern(
+            regexp = "^(03|05|07|08|09)[0-9]{8}$",
+            message = "Số điện thoại không hợp lệ"
+    )
+    private String phone;
 
     public CandidateCreateDTO() {
     }
@@ -54,5 +61,13 @@ public class CandidateCreateDTO {
 
     public void setYearsOfExperience(Integer yearsOfExperience) {
         this.yearsOfExperience = yearsOfExperience;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
